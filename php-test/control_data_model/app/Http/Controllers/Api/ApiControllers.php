111c29c325255a727dbec0c38f388dbaf60e5a99
AntsugDev <antonio.sugamele@gmail.com>
2025-11-12 19:01:07 +0100

    Script laravel-php per confronto

--- php-test/control_data_model/app/Http/Controllers/Api/ApiControllers.php
+++ php-test/control_data_model/app/Http/Controllers/Api/ApiControllers.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers\Api;
 use App\Utils\Utils;
+use App\Http\Controllers\Controller;
 
 
 class ApiControllers extends Controller{
 
 
-public function __construct(Utils $utils){
-$utils = new Utils();
-}
 
-    pubic function index(){
 
-            $data = $this->utils->getData();
-            return response()->json($data);
+    public function index(){
+            $utils = new Utils();
+            $data = $utils->getData();
+            $sogliaMinima = $utils->getSoglia();
+            return response()->json([
+            "soglia_reddito" => number_format($sogliaMinima,2,',','.'),
+            "data_model"=>$data
+            ]);
 
 
     }
--- php-test/control_data_model/app/Utils/Utils.php
+++ php-test/control_data_model/app/Utils/Utils.php
@@ -27,21 +27,42 @@ class Utils
     {
         try {
             $result = new \Illuminate\Support\Collection;
-            $this->model->each(function ($item) use (&$result) {
+            $count = 1;
+            $this->model->each(function ($item) use (&$result, &$count) {
 
-                $importo = $this->getImport($item->costo_auto, ! boolval($item->nuovo_usato));
+                $importo = $this->getImport($item->costo_auto, !boolval($item->nuovo_usato));
                 $rata = $this->getRata(intval($item->nr_rate), $importo, floatval($item->tan));
                 $sostenibilita = $this->sostenibilita(floatval($item->diff_reddito), $rata);
                 $coefficienteK = $this->coefficenteK($sostenibilita, $item->nr_rate);
+                $Rincome = $this->RIncome(floatval($item->diff_reddito));
+                $Ranticipo = $this->Ranticipo(!boolval($item->nuovo_usato), floatval($item->anticipo*100));
+                $Re = $this->RE($Rincome,$Ranticipo);
+                $Rs = $this->RS($sostenibilita);
+                $Rd = $this->RD(!boolval($item->patentato), intval($item->nr_figli));
+                $decisione = $this->score_decisionale($Re,$Rs,$Rd,$sostenibilita, !boolval($item->nuovo_usato),$coefficienteK,floatval($item->costo_auto), intval($item->nr_rate));
                 $result->add([
+                    'id' => $count,
+                    'reddito'=>floatval($item->diff_reddito),
+                    'Rincome' =>$Rincome,
                     'costo_auto'=>$item->costo_auto,
                     'formula'=> boolval($item->nuovo_usato) ? 'usata' :'nuova' ,
+                    'anticipo'=>floatval($item->anticipo*100),
+                    'Ranticipo'=> $Ranticipo,
                     'importo' => $importo,
+                    'nrRate'=>$item->nr_rate,
                     'rata' => $rata,
                     'sostenibilita' => $sostenibilita,
                     'coefficienteK' => $coefficienteK,
+                    'RE'=>$Re,
+                    'RS'=>$Rs,
+                    'RD'=> $Rd,
+                    'Decisione_finale' => $decisione
+                    
                 ]);
+                $count ++;
             });
+			
+			return $result;
 
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
@@ -49,12 +70,88 @@ class Utils
 
     }
 
-    public function getSoglia(): float|int
+    public function getSoglia()
     {
 
         return $this->avg + ($this->avg * ($this->min / $this->max));
     }
-
+    
+    public function RIncome(float $reddito){
+    
+    $soglia = $this->getSoglia();
+       if($reddito >= $soglia) 
+        return 1;
+       else if($reddito >= ($soglia *0.8))
+       return 2;
+        else if($reddito >= ($soglia *0.6))
+       return 3;
+         else if($reddito >= ($soglia *0.4))
+       return 4;
+       else return 5;
+    }
+    
+    public function RE(int $Rincome, int $Ranticipo){
+    
+    $re =  (0.8*$Rincome)+(0.2*$Ranticipo);
+    if($re <= 1.5 ) return 1;
+    else if($re <= 1.5 ) return 1;
+    else if($re <= 2.4 ) return 2;
+    else if($re <= 3.2 ) return 3;
+    else if($re <= 4 ) return 4;
+    else return 5;
+    
+    }
+    
+    public function Ranticipo(bool $formula, float $anticipo){
+    if(!$formula) return 1;
+    else{
+        if($anticipo >= 25) return 1;
+        else if($anticipo >= 15 && $anticipo <=24) return 2;
+        else if($anticipo >= 5 && $anticipo <=14) return 3;
+        else return 4;
+    }
+    }
+    
+    public function RS(float $sostenibilita){
+        if($sostenibilita <= 0.15) return 1;
+        else if($sostenibilita >= 0.16 && $sostenibilita <= 0.20) return 2;
+        else if($sostenibilita >= 0.21  && $sostenibilita <= 0.29) return 3;
+        else if($sostenibilita >= 0.30 && $sostenibilita  <= 0.34) return 4;
+        else return 5;
+    
+    }
+    
+    public function RD(bool $neoPatentati, int $nrFigli){
+    $base = 0;
+    if($nrFigli === 0) $base = 1;
+    else if($nrFigli ===1) $base =2;
+    else if($nrFigli ===2) $base =3;
+    else $base= 4;
+    
+    if($neoPatentati) $base +=1;
+    return $base;
+    }
+    
+    public function score_decisionale(int $RE, int $RS, int $RD, float $sostenibilita, bool $formula, array $K, float $costoAuto, int $nrRate){
+    
+     $score = ($RE*0.5)+($RS*0.3)+($RD*0.2);
+    if($sostenibilita >= 0.35) return "Non concedibile";
+    else if($sostenibilita >= 0.21 && $sostenibilita <= 0.34 && !$formula){
+        $importoDaAnticipare =array_key_exists(2,$K) && $K[2] >0 ?  $costoAuto *($K[2]/100) : 0;
+        $nrRateT  = array_key_exists(3,$K) && $K[3] >0 ? $nrRate+$K[3] : $nrRate;
+        return "Simulazione sceniario nuovo dove devo verificare e studiare se dando un anticipo di ".number_format($importoDaAnticipare,2,',','.')." euro, oppure aumentando le rate fino a $nrRateT cosa cambia ...";
+    }else {
+        if($score <= 1.5) return "Bonifico";
+        else if($score >= 1.6 && $score < 4) return $formula ? 'Finanziamento a 3 anni' : 'Finanziamento Classico';
+        else if($score >= 4 && $score < 5)  return 'Finanziamento con riserva (da simulare e dipendente da K)';
+        else return "Non concedibile";
+    }
+    
+   
+    
+    }
+    
+    
     public function getImport(float $reddito, bool $formula = true): float|int
     {
         if ($formula) {
@@ -96,24 +193,25 @@ class Utils
     public function coefficenteK(float $sostenibilita, int $nrRate)
     {
         if ($nrRate === 0) {
-            return [0, 'Non calcolabile'];
+            return [0, 'Non calcolabile',0,0];
         }
         if ($sostenibilita <= 0.20) {
             return [1.0, 'Ottima'];
         } elseif ($sostenibilita >= 0.35) {
-            return [round($sostenibilita / 0.20), 'Non concedibile'];
+            return [round($sostenibilita / 0.20), 'Non concedibile',0,0];
         } else {
             $k = $sostenibilita / 0.20;
-            $anticipo = round(($k - 1) * 10, 1);
-            $nrRatePlus = intval(12 * ($k - 1));
+            //todo da rivedere o troppo alti i valori o troppo bassi
+            $anticipo = round(($k - 1) * 80, 1);
+            $nrRatePlus = intval(24 * ($k - 1));
             if ($k <= 1.20) {
-                return [$k, "Azione: anticipo del $anticipo%"];
+                return [$k, "Azione: anticipo del $anticipo%", $anticipo, 0];
             } elseif ($k <= 1.40) {
-                return [$k, "Azione: anticipo del $anticipo% o $nrRatePlus rate in più"];
+                return [$k, "Azione: anticipo del $anticipo% o $nrRatePlus rate in più", $anticipo, $nrRatePlus];
             } elseif ($k <= 1.70) {
-                return [$k, "Azione: anticipo del $anticipo% o $nrRatePlus rate in più"];
+                return [$k, "Azione: anticipo del $anticipo% o $nrRatePlus rate in più",$anticipo, $nrRatePlus];
             } else {
-                return [$k, 'Non sostenibile'];
+                return [$k, 'Non sostenibile',0,0];
             }
         }
     }
