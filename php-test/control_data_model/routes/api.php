0151a101aaf69765b7b44d75b44ca362b4efa2e4
AntsugDev <antonio.sugamele@gmail.com>
2025-11-19 19:01:37 +0100

    Sistemazione directory

--- php-test/control_data_model/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ApiControllers;
-
-Route::get('ctrl_test', [ApiControllers::class,'index']);
-Route::post('simulazione', [ApiControllers::class,'simulazione']);
